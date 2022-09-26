4a20fc795c333a64b0bd27894de3098b30376dd7
Hameedullah Khan <hameed@sudoconsultants.com>
2022-09-26 14:44:00 +0400

    Add json list output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "container_definition_json" {
   description = "The coantiner defintion template"
   value       = jsonencode(local.container_definition)
 }
+
+output "container_definition_json_list" {
+  description = "The coantiner defintion template as json encoded list"
+  value       = jsonencode([local.container_definition])
+}
